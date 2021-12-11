814ced8a2e2e9f81b2a00448e678fcef0d623c34
maurigamg <44620109+maurigamg@users.noreply.github.com>
2021-12-10 23:41:26 -0600

    All requirements implemented
    ELT is working, may be it's necesary some improves.

--- CLI tool - ELT/datajr.py
+++ CLI tool - ELT/datajr.py
@@ -1,23 +1,12 @@
 import sys
 import requests
 import sqlite3
-from datetime import datetime
+import pandas as pd
 
 """
-author: Mauricio Gamboa
+Author: Mauricio Gamboa
 Data Engineering Jr Challenge
 """
-
-def validate_day(day):
-    # checking if format matches the date
-    pformat = "%Y-%m-%d"
-    res = True
-    # using try-except to check for truth value
-    try:
-        res = bool(datetime.strptime(day, pformat))
-    except ValueError:
-        res = False
-    return res
 
 #This function receives a day and returns the information extracted from the API according to the day
 def extract_data(day):
@@ -55,13 +44,15 @@ def transform_song(song):
     #Get non-null variable
     song_id = song["song_id"]
     released_at = song["released_at"]
+    released_at = str(pd.Timestamp(released_at)) #ISO8601 strings (YYYY-MM-DD HH:MM:SS.SSS)
     duration_seconds = to_seconds(song["duration"]) #Duration to seconds
     artist = song["artist"]
     name = song["name"]
     last_played_at = times_played = global_rank = "NULL"
 
-    if(song.get("stats") != None):
+    if(song.get("stats") != None): #If there are statistics
         last_played_at = song["stats"]["last_played_at"]
+        last_played_at = str(pd.Timestamp(last_played_at))
         times_played = song["stats"]["times_played"]
         global_rank = song["stats"]["global_rank"]
     
@@ -73,7 +64,7 @@ def load_data(data):
     cur = con.cursor()
     
     completed_rows = 0
-    integrity_error = 0
+    integrity_errors = 0
 
     #Instruction to load the each song
     instruction = ("insert into song (song_id, released_at, duration_seconds, "
@@ -82,22 +73,21 @@ def load_data(data):
 
     for info_song in data:
         transformed = transform_song(info_song) #Transformation of a song
-        print(transformed)
         try:
             with con: # Connection object used as context manager (do commit or rollback automatically)
-                continue;
                 cur.execute(instruction, transformed) #Load transformed song
-                complete += 1
+                completed_rows += 1
         except sqlite3.OperationalError:
             print("No such table: song")
             break;
-        except sqlite3.IntegrityError:
-            integrity_error += 1
+        except sqlite3.IntegrityError: #Inserting a same song_id
+            integrity_errors += 1
 
     con.close()
 
+    #Just to see load resuld
     print("Loaded songs:", completed_rows)
-    print("Failed inserts (integrity):", integrity_error)
+    print("Failed inserts (integrity):", integrity_errors)
 
 def main():
     if len(sys.argv) == 2:
